c3d1c34e91674d94767513a5191c2bcc28206494
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-09-03 12:07:42 +0200

    Adding deafult text style, colors & creating AppText component

---
+++ app/config/colors.js
@@ -0,0 +1,10 @@
+export default {
+  black: "#090909",
+  danger: "#FF5252",
+  dark: "#34404A",
+  light: "#F4F4F4",
+  medium: "#696969",
+  primary: "#214951",
+  secondary: "#497179",
+  white: "#FFFFFF",
+};
