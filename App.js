4cb1314514ddca4a68c574f8bf30ba07282d0705
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-10-07 14:38:12 +0200

    Cleaning App.js & creating AuthStack.navigation.js

--- App.js
+++ App.js
@@ -1,58 +1,14 @@
-import { ImageBackground, StyleSheet, View, Text } from "react-native";
-import AppButton from "./app/components/AppButton";
-import * as Yup from "yup";
+import { StyleSheet, View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import SubmitButton from "./app/components/forms/SubmitButton";
-import AppText from "./app/components/AppText";
-import AppForm from "./app/components/forms/AppForm";
-import AppFormField from "./app/components/forms/AppFormField";
-import Screen from "./app/components/Screen";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import colors from "./app/config/colors";
-
-const validationSchema = Yup.object().shape({
-  test: Yup.string().required().min(1).label("Test"),
-});
-
-const Stack = createNativeStackNavigator();
+import AuthStack from "./app/navigation/AuthStack.navigation";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={RegisterScreen}
-          options={{
-            headerTitle: "Inscription",
-            headerTitleStyle: {
-              fontSize: 22,
-            },
-            headerBackTitle: "Connexion",
-            headerBackTitleVisible: false,
-            headerTransparent: true,
-            headerTintColor: colors.white,
-          }}
-        />
-      </Stack.Navigator>
+      <AuthStack />
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 10,
-    width: "100%",
-  },
-});
+const styles = StyleSheet.create({});
