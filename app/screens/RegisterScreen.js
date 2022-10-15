56e1b63162f10bcd4692c7d8d77b580ef97d7009
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-10-15 15:38:42 +0200

    Keyboard don't hide textinput on registerscreen

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -4,9 +4,11 @@ import {
   View,
   StyleSheet,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import * as Yup from "yup";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 import Screen from "../components/Screen";
 import { AppForm, AppFormField, SubmitButton } from "../components/forms";
@@ -50,96 +52,101 @@ function RegisterScreen(props) {
       style={{ flex: 1 }}
     >
       <Screen style={styles.container}>
-        <View style={styles.formContainer}>
-          <AppForm
-            initialValues={{
-              lastName: "",
-              firstName: "",
-              email: "",
-              birthDate: "",
-              password: "",
-              passwordConfirmation: "",
-            }}
-            onSubmit={(values) => console.log(values)}
-            validationSchema={validationSchema}
-          >
-            <AppFormField
-              autoCapitalize="words"
-              autoCorrect={true}
-              keyboardAppearance={colors.colorScheme}
-              keyboardType="default"
-              name="lastName"
-              placeholder="Nom"
-              registration
-              textContentType="none"
-            />
-            <AppFormField
-              autoCapitalize="words"
-              autoCorrect={true}
-              keyboardAppearance={colors.colorScheme}
-              keyboardType="default"
-              name="firstName"
-              placeholder="Prénom"
-              registration
-              textContentType="none"
-            />
-            <AppFormField
-              birthDate
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardAppearance={colors.colorScheme}
-              keyboardType="default"
-              name="birthDate"
-              placeholder="Date de naissance (JJ/MM/AAAA)"
-              registration
-              textContentType="none"
-            />
-            <AppFormField
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardAppearance={colors.colorScheme}
-              keyboardType="email-address"
-              name="email"
-              placeholder="Adresse email"
-              registration
-              textContentType="emailAddress"
-            />
-            <AppFormField
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardAppearance={colors.colorScheme}
-              name="password"
-              placeholder="Mot de passe"
-              registration
-              secureTextEntry
-              textContentType="password"
-            />
-            <AppFormField
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardAppearance={colors.colorScheme}
-              name="passwordConfirmation"
-              placeholder="Confirmation du mot de passe"
-              registration
-              secureTextEntry
-              textContentType="password"
-            />
-            <SubmitButton title="Inscription" />
-          </AppForm>
-          <TouchableOpacity
-            onPress={() => console.log("To data usage informations")}
-            style={styles.dataUsageContainer}
-          >
-            <MaterialCommunityIcons
-              name="information-outline"
-              size={22}
-              color={colors.buttonPrimary}
-            />
-            <AppText style={styles.dataUsageText}>
-              Comment sont utilisées mes données ?
-            </AppText>
-          </TouchableOpacity>
-        </View>
+        <KeyboardAwareScrollView
+          style={styles.scrollView}
+          showsVerticalScrollIndicator={false}
+        >
+          <View style={styles.formContainer}>
+            <AppForm
+              initialValues={{
+                lastName: "",
+                firstName: "",
+                email: "",
+                birthDate: "",
+                password: "",
+                passwordConfirmation: "",
+              }}
+              onSubmit={(values) => console.log(values)}
+              validationSchema={validationSchema}
+            >
+              <AppFormField
+                autoCapitalize="words"
+                autoCorrect={true}
+                keyboardAppearance={colors.colorScheme}
+                keyboardType="default"
+                name="lastName"
+                placeholder="Nom"
+                registration
+                textContentType="none"
+              />
+              <AppFormField
+                autoCapitalize="words"
+                autoCorrect={true}
+                keyboardAppearance={colors.colorScheme}
+                keyboardType="default"
+                name="firstName"
+                placeholder="Prénom"
+                registration
+                textContentType="none"
+              />
+              <AppFormField
+                birthDate
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardAppearance={colors.colorScheme}
+                keyboardType="default"
+                name="birthDate"
+                placeholder="Date de naissance (JJ/MM/AAAA)"
+                registration
+                textContentType="none"
+              />
+              <AppFormField
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardAppearance={colors.colorScheme}
+                keyboardType="email-address"
+                name="email"
+                placeholder="Adresse email"
+                registration
+                textContentType="emailAddress"
+              />
+              <AppFormField
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardAppearance={colors.colorScheme}
+                name="password"
+                placeholder="Mot de passe"
+                registration
+                secureTextEntry
+                textContentType="password"
+              />
+              <AppFormField
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardAppearance={colors.colorScheme}
+                name="passwordConfirmation"
+                placeholder="Confirmation du mot de passe"
+                registration
+                secureTextEntry
+                textContentType="password"
+              />
+              <SubmitButton title="Inscription" />
+            </AppForm>
+            <TouchableOpacity
+              onPress={() => console.log("To data usage informations")}
+              style={styles.dataUsageContainer}
+            >
+              <MaterialCommunityIcons
+                name="information-outline"
+                size={22}
+                color={colors.buttonPrimary}
+              />
+              <AppText style={styles.dataUsageText}>
+                Comment sont utilisées mes données ?
+              </AppText>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAwareScrollView>
       </Screen>
     </ImageBackground>
   );
@@ -149,7 +156,6 @@ const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     flex: 1,
-    height: "100%",
     justifyContent: "center",
     paddingHorizontal: 10,
     width: "100%",
@@ -167,7 +173,10 @@ const styles = StyleSheet.create({
     flex: 1,
     height: "100%",
     justifyContent: "flex-start",
-    marginBottom: 100,
+    width: "100%",
+  },
+  scrollView: {
+    flex: 1,
     width: "100%",
   },
 });
