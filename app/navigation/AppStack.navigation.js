be1d1e01edfab390f6b508c7781108bdfded4757
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-10-17 15:34:08 +0200

    First part of the navigation tab bar

--- app/navigation/AppStack.navigation.js
+++ app/navigation/AppStack.navigation.js
@@ -1,10 +1,109 @@
 import React from "react";
 import { StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 
 import LogoutTestScreen from "../screens/LogoutTestScreen";
 
+const Tab = createBottomTabNavigator();
+
 function AppStack(props) {
-  return <LogoutTestScreen />;
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Events"
+        component={LogoutTestScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Ionicons
+              name="home-outline"
+              size={30}
+              color={color}
+              style={{ paddingTop: 5 }}
+            />
+          ),
+          tabBarLabel: "Mes évènements",
+          tabBarLabelStyle: {
+            fontSize: 10,
+            paddingBottom: 5,
+          },
+          tabBarStyle: {
+            height: 60,
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Join"
+        component={LogoutTestScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Ionicons
+              name="arrow-redo-circle-outline"
+              size={35}
+              color={color}
+              style={{ paddingTop: 5 }}
+            />
+          ),
+          tabBarLabel: "Rejoindre",
+          tabBarLabelStyle: {
+            fontSize: 10,
+            paddingBottom: 5,
+          },
+          tabBarStyle: {
+            height: 60,
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Create"
+        component={LogoutTestScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Ionicons
+              name="add-circle-outline"
+              size={35}
+              color={color}
+              style={{ paddingTop: 5 }}
+            />
+          ),
+          tabBarLabel: "Créer",
+          tabBarLabelStyle: {
+            fontSize: 10,
+            paddingBottom: 5,
+          },
+          tabBarStyle: {
+            height: 60,
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={LogoutTestScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Ionicons
+              name="settings-outline"
+              size={30}
+              color={color}
+              style={{ paddingTop: 5 }}
+            />
+          ),
+          tabBarLabel: "Paramètres",
+          tabBarLabelStyle: {
+            fontSize: 10,
+            paddingBottom: 5,
+          },
+          tabBarStyle: {
+            height: 60,
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
 
 const styles = StyleSheet.create({});
--- app/screens/LogoutTestScreen.js
+++ app/screens/LogoutTestScreen.js
@@ -6,25 +6,28 @@ import jwt_decode from "jwt-decode";
 import { AuthContext } from "../auth/AuthContext";
 import AppText from "../components/AppText";
 import LoadingIndicator from "../components/LoadingIndicator";
+import Screen from "../components/Screen";
 
 function LogoutTestScreen(props) {
   const { logout, isLoading, userAccessToken, userRefreshToken } =
     useContext(AuthContext);
   const userInfos = jwt_decode(userAccessToken);
   return (
-    <View style={styles.container}>
-      <AppText>
-        Bonjour{" "}
-        {userInfos.firstName.charAt(0).toUpperCase() +
-          userInfos.firstName.slice(1)}
-      </AppText>
-      <AppButton
-        title="Logout"
-        onPress={() => logout(userRefreshToken)}
-        style={{ width: "90%" }}
-      />
-      {isLoading && <LoadingIndicator />}
-    </View>
+    <Screen version="scroll">
+      <View style={styles.container}>
+        <AppText>
+          Bonjour{" "}
+          {userInfos.firstName.charAt(0).toUpperCase() +
+            userInfos.firstName.slice(1)}
+        </AppText>
+        <AppButton
+          title="Logout"
+          onPress={() => logout(userRefreshToken)}
+          style={{ width: "90%" }}
+        />
+        {isLoading && <LoadingIndicator />}
+      </View>
+    </Screen>
   );
 }
 
