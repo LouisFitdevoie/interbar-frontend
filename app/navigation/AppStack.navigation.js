e871e9fa0f173d589416e57bec63d529bc900b9b
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-10-17 15:01:31 +0200

    Clearing code for bottom tab navigator creation

--- app/navigation/AppStack.navigation.js
+++ app/navigation/AppStack.navigation.js
@@ -1,39 +1,12 @@
-import React, { useContext } from "react";
-import { View, StyleSheet } from "react-native";
-import AppButton from "../components/AppButton";
-import jwt_decode from "jwt-decode";
+import React from "react";
+import { StyleSheet } from "react-native";
 
-import { AuthContext } from "../auth/AuthContext";
-import AppText from "../components/AppText";
-import LoadingIndicator from "../components/LoadingIndicator";
+import LogoutTestScreen from "../screens/LogoutTestScreen";
 
 function AppStack(props) {
-  const { logout, isLoading, userAccessToken, userRefreshToken } =
-    useContext(AuthContext);
-  const userInfos = jwt_decode(userAccessToken);
-  return (
-    <View style={styles.container}>
-      <AppText>
-        Bonjour{" "}
-        {userInfos.firstName.charAt(0).toUpperCase() +
-          userInfos.firstName.slice(1)}
-      </AppText>
-      <AppButton
-        title="Logout"
-        onPress={() => logout(userRefreshToken)}
-        style={{ width: "90%" }}
-      />
-      {isLoading && <LoadingIndicator />}
-    </View>
-  );
+  return <LogoutTestScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
 
 export default AppStack;
