e10fb61df7909aab3f67df5157c6050624da5783
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-10-21 16:11:05 +0200

    Using the new user state from AuthContext instead of decoding the access token now

--- app/screens/LogoutTestScreen.js
+++ app/screens/LogoutTestScreen.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { View, StyleSheet } from "react-native";
 import AppButton from "../components/AppButton";
-import jwt_decode from "jwt-decode";
 
 import { AuthContext } from "../auth/AuthContext";
 import AppText from "../components/AppText";
@@ -9,22 +8,15 @@ import LoadingIndicator from "../components/LoadingIndicator";
 import Screen from "../components/Screen";
 
 function LogoutTestScreen(props) {
-  const {
-    logout,
-    isLoading,
-    userAccessToken,
-    isTokenExpired,
-    updateAccessToken,
-  } = useContext(AuthContext);
-  const userInfos = jwt_decode(userAccessToken);
+  const { logout, isLoading, isTokenExpired, updateAccessToken, user } =
+    useContext(AuthContext);
 
   return (
     <Screen version="scroll">
       <View style={styles.container}>
         <AppText>
           Bonjour{" "}
-          {userInfos.firstName.charAt(0).toUpperCase() +
-            userInfos.firstName.slice(1)}
+          {user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1)}
           {`\n`}Access token still valid ? {isTokenExpired() ? "No" : "Yes"}
         </AppText>
         <AppButton
