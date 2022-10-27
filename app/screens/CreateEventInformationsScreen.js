909aa38a482c3dc1d1781ff55d1a8220b7a754a6
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-10-27 16:46:01 +0200

    Editing the CreateEventInformationScreen

--- app/screens/CreateEventInformationsScreen.js
+++ app/screens/CreateEventInformationsScreen.js
@@ -7,10 +7,21 @@ import Screen from "../components/Screen";
 function CreateEventInformationsScreen({ navigation }) {
   return (
     <Screen style={styles.container}>
-      <AppText>Test</AppText>
+      <AppText style={styles.text}>
+        Pour créer un évènement, vous devrez passer par plusieurs étapes.
+      </AppText>
+      <AppText style={styles.text}>
+        Vous devrez d'abord donner des informations générales sur l'évènement,
+        comme son nom, la date de début ou la date de fin.
+      </AppText>
+      <AppText style={styles.text}>
+        Ensuite, vous devrez créer le tarif de l'évènement en ajoutant des
+        produits existants ou en en créant de nouveaux.
+      </AppText>
       <AppButton
-        title="Créer un évènement"
+        title="Commencer"
         onPress={() => navigation.navigate("CreateEvent")}
+        style={{ width: "95%", marginTop: 20 }}
       />
     </Screen>
   );
@@ -18,8 +29,14 @@ function CreateEventInformationsScreen({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
     flex: 1,
-    marginHorizontal: 10,
+    width: "100%",
+  },
+  text: {
+    marginTop: 10,
+    textAlign: "left",
+    width: "90%",
   },
 });
 
