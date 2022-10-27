e61fb3359e23edbb5075869d4ab0b61df78d5303
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-10-27 16:28:38 +0200

    Adding the ability to cancel the creation of an event

--- app/navigation/CreateEventStack.navigation.js
+++ app/navigation/CreateEventStack.navigation.js
@@ -3,10 +3,12 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CreateEventInformationsScreen from "../screens/CreateEventInformationsScreen";
 import CreateEventScreen from "../screens/CreateEventScreen";
 import colors from "../config/colors";
+import { HeaderBackButton } from "@react-navigation/elements";
+import { Alert } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
-export default function CreateEventStack() {
+export default function CreateEventStack({ navigation }) {
   return (
     <Stack.Navigator>
       <Stack.Screen
@@ -25,10 +27,35 @@ export default function CreateEventStack() {
         options={{
           headerShown: true,
           headerTitle: "Créer un évènement",
-          headerBackTitle: "Retour",
-          headerBackTitleVisible: false,
+          headerTitleStyle: {
+            fontSize: 22,
+          },
           headerTransparent: true,
           headerTintColor: colors.primary,
+          headerLeft: () => (
+            <HeaderBackButton
+              tintColor={colors.primary}
+              onPress={() => {
+                Alert.alert(
+                  "Voulez-vous vraiment annuler la création de l'évènement ?",
+                  "Toutes les informations saisies seront perdues.",
+                  [
+                    {
+                      text: "Annuler",
+                      style: "cancel",
+                    },
+                    {
+                      text: "Confirmer",
+                      style: "destructive",
+                      onPress: () => {
+                        navigation.navigate("CreateEventInformations");
+                      },
+                    },
+                  ]
+                );
+              }}
+            />
+          ),
         }}
       />
     </Stack.Navigator>
