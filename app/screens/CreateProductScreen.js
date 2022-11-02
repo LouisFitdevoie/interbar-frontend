60d3ef9d6125162af0cb91856c0240155c1ec415
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-11-02 16:06:34 +0100

    Adding the validator for the creation of products

--- app/screens/CreateProductScreen.js
+++ app/screens/CreateProductScreen.js
@@ -1,30 +1,22 @@
 import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
+
 import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 import RadioButtonForm from "../components/forms/RadioButtonForm";
 import Screen from "../components/Screen";
+import createProductValidator from "../validators/createProduct.validator.js";
 
 function CreateProductScreen(props) {
-  const [productFound, setProductFound] = useState(false);
-  const [product, setProduct] = useState({
-    name: "",
-    description: "",
-    category: "",
-  });
-
   return (
     <Screen style={styles.container}>
       <AppForm
         initialValues={{
-          name: product.name,
-          category: product.category,
-          buyingPrice: "",
-          sellingPrice: "",
-          stock: "",
-          description: product.description,
+          name: "",
+          category: "",
+          description: "",
         }}
         onSubmit={(values) => console.log(values)}
-        validationSchema={null}
+        validationSchema={createProductValidator}
       >
         <AppFormField
           autoCapitalize="words"
@@ -35,53 +27,22 @@ function CreateProductScreen(props) {
           placeholder="Nom du produit"
           textContentType="none"
         />
-        {productFound && (
-          <>
-            <RadioButtonForm name="category" value="food" label="Nourriture" />
-            <RadioButtonForm
-              name="category"
-              value="alcohol"
-              label="Boisson alcoolisée"
-            />
-            <RadioButtonForm name="category" value="soft" label="Soft" />
-            <AppFormField
-              autoCapitalize="sentences"
-              autoCorrect={true}
-              keyboardAppearance={colors.colorScheme}
-              keyboardType="default"
-              name="description"
-              placeholder="Description (facultatif)"
-              textContentType="none"
-            />
-            <AppFormField
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardAppearance={colors.colorScheme}
-              keyboardType="decimal-pad"
-              name="buyingPrice"
-              placeholder="Prix d'achat"
-              textContentType="none"
-            />
-            <AppFormField
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardAppearance={colors.colorScheme}
-              keyboardType="decimal-pad"
-              name="sellingPrice"
-              placeholder="Prix de vente"
-              textContentType="none"
-            />
-            <AppFormField
-              autoCapitalize="none"
-              autoCorrect={false}
-              keyboardAppearance={colors.colorScheme}
-              keyboardType="number-pad"
-              name="stock"
-              placeholder="Stock"
-              textContentType="none"
-            />
-          </>
-        )}
+        <RadioButtonForm name="category" value="food" label="Nourriture" />
+        <RadioButtonForm
+          name="category"
+          value="alcohol"
+          label="Boisson alcoolisée"
+        />
+        <RadioButtonForm name="category" value="soft" label="Soft" />
+        <AppFormField
+          autoCapitalize="sentences"
+          autoCorrect={true}
+          keyboardAppearance={colors.colorScheme}
+          keyboardType="default"
+          name="description"
+          placeholder="Description (facultatif)"
+          textContentType="none"
+        />
         <SubmitButton title="Valider" disabled={productFound} />
       </AppForm>
     </Screen>
