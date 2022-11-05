f1efbeeab49fed474f0553636781559a059bfc79
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-11-05 16:00:58 +0100

    New userEvent API calls added

--- app/api/userEvent.api.js
+++ app/api/userEvent.api.js
@@ -16,3 +16,39 @@ exports.userCreateEvent = (eventId, userId, userAccessToken) => {
     },
   });
 };
+
+exports.userJoinEvent = (eventId, userId, userAccessToken) => {
+  return axios({
+    method: "post",
+    url: `${BASE_URL}/user-join-event`,
+    data: {
+      eventId,
+      userId,
+      role: 0,
+    },
+    headers: {
+      Authorization: `Bearer ${userAccessToken}`,
+    },
+  });
+};
+
+exports.sellerJoinEvent = (
+  eventId,
+  userId,
+  sellerPassword,
+  userAccessToken
+) => {
+  return axios({
+    method: "post",
+    url: `${BASE_URL}/user-join-event`,
+    data: {
+      eventId,
+      userId,
+      sellerPassword,
+      role: 1,
+    },
+    headers: {
+      Authorization: `Bearer ${userAccessToken}`,
+    },
+  });
+};
