2f71acc84336c83b3846964680b88386144bd11c
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-11-06 16:50:20 +0100

    Correcting an error that didn't autoplayed the LoadingIndicator

--- app/components/LoadingIndicator.js
+++ app/components/LoadingIndicator.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { View, StyleSheet } from "react-native";
 import LottieView from "lottie-react-native";
 
 const indicator = require("../assets/lottie-animations/loading-indicator.json");
 
 function LoadingIndicator(props) {
+  const animationRef = useRef();
+
+  useEffect(() => {
+    if (animationRef.current) {
+      setTimeout(() => {
+        animationRef.current?.play();
+      }, 0);
+    }
+  }, [animationRef.current]);
   return (
     <View style={styles.container}>
       <View style={styles.background}>
-        <LottieView autoPlay style={styles.indicator} source={indicator} />
+        <LottieView
+          ref={animationRef}
+          autoPlay
+          style={styles.indicator}
+          source={indicator}
+        />
       </View>
     </View>
   );
