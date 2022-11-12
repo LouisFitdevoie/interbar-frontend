40f1c628791de3a6d1cafa21878fe5dfd5d22c01
LouisFitd <65094430+LouisFitdevoie@users.noreply.github.com>
2022-11-12 13:50:06 +0100

    Creating a component for the sort menu used in the differents events screens

---
+++ app/components/SortMenu.js
@@ -0,0 +1,157 @@
+import React, { useState } from "react";
+import { View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+import AppText from "./AppText";
+import RadioButton from "./RadioButton";
+
+function SortMenu({
+  sortOptionSelected,
+  setSortOptionSelected,
+  scrollOptionSelected,
+  setScrollOptionSelected,
+  sortOptions,
+  scrollOptions,
+  defaultItems,
+  displayedItems,
+  setDisplayedItems,
+}) {
+  const [isSortOptionsVisible, setIsSortOptionsVisible] = useState(false);
+
+  const handleSortOptionChanged = (option) => {
+    setSortOptionSelected(option);
+    setIsSortOptionsVisible(false);
+  };
+
+  const handleScrollOptionChanged = (option) => {
+    setScrollOptionSelected(option);
+  };
+
+  return (
+    <View style={styles.sortMenuContainer}>
+      <View style={styles.sortView}>
+        <View style={styles.sortTitle}>
+          <AppText style={{ fontSize: 20 }}>
+            {
+              sortOptions.find((option) => option.value === sortOptionSelected)
+                .name
+            }{" "}
+            ({displayedItems.length})
+          </AppText>
+          <TouchableOpacity
+            style={[
+              styles.sortButton,
+              {
+                backgroundColor: isSortOptionsVisible
+                  ? colors.buttonPrimary
+                  : colors.white,
+              },
+            ]}
+            onPress={() => setIsSortOptionsVisible(!isSortOptionsVisible)}
+          >
+            <MaterialCommunityIcons
+              name="filter-variant"
+              size={30}
+              color={isSortOptionsVisible ? colors.white : colors.buttonPrimary}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.sortOptionsView}>
+          {isSortOptionsVisible && (
+            <View style={styles.sortOptions}>
+              {sortOptions.map((sortOption) => (
+                <View key={sortOption.value}>
+                  <TouchableOpacity
+                    key={sortOption.value}
+                    onPress={() => handleSortOptionChanged(sortOption.value)}
+                  >
+                    <AppText
+                      style={
+                        sortOptionSelected === sortOption.value
+                          ? styles.optionSelected
+                          : styles.option
+                      }
+                    >
+                      {sortOption.name}
+                    </AppText>
+                  </TouchableOpacity>
+                  {sortOption.value !==
+                    sortOptions[sortOptions.length - 1].value && (
+                    <View style={styles.separator} />
+                  )}
+                </View>
+              ))}
+            </View>
+          )}
+        </View>
+      </View>
+      <ScrollView horizontal>
+        {scrollOptions.map((scrollOption) => (
+          <RadioButton
+            key={scrollOption.value}
+            value={scrollOption.value}
+            onPress={() => handleScrollOptionChanged(scrollOption.value)}
+            label={scrollOption.name}
+            stateValue={scrollOptionSelected}
+            style={{ paddingRight: 20 }}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  option: {
+    padding: 5,
+    width: "100%",
+    textAlign: "center",
+    color: colors.light,
+  },
+  optionSelected: {
+    padding: 5,
+    fontSize: 20,
+    width: "100%",
+    textAlign: "center",
+    color: colors.white,
+    fontWeight: "bold",
+  },
+  separator: {
+    height: 1,
+    width: "100%",
+    backgroundColor: colors.light,
+    alignSelf: "center",
+  },
+  sortButton: {
+    padding: 5,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  sortMenuContainer: {
+    marginBottom: 0,
+    paddingHorizontal: 10,
+  },
+  sortOptionsView: {
+    backgroundColor: colors.buttonPrimary,
+    borderRadius: 20,
+    borderTopRightRadius: 0,
+    width: 250,
+  },
+  sortTitle: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  sortView: {
+    width: "100%",
+    flexDirection: "column",
+    alignItems: "flex-end",
+  },
+  sortOptions: {
+    padding: 5,
+  },
+});
+
+export default SortMenu;
--- app/screens/EventsTab/UserCurrentEventScreen.js
+++ app/screens/EventsTab/UserCurrentEventScreen.js
@@ -7,6 +7,7 @@ import colors from "../../config/colors";
 import AppText from "../../components/AppText";
 import RadioButton from "../../components/RadioButton";
 import AppButton from "../../components/AppButton";
+import SortMenu from "../../components/SortMenu";
 
 function UserCurrentEventScreen({
   navigation,
@@ -38,90 +39,19 @@ function UserCurrentEventScreen({
     { name: "Non payées", value: "unpaid" },
   ];
 
-  const handleSortOptionChanged = (option) => {
-    setSortOptionSelected(option);
-    setIsSortOptionsVisible(false);
-  };
-
-  const handlePaidOptionChanged = (option) => {
-    setPaidOptionSelected(option);
-  };
-
   return (
     <Screen style={styles.container}>
-      <View style={styles.sortMenuContainer}>
-        <View style={styles.sortView}>
-          <View style={styles.sortTitle}>
-            <AppText style={{ fontSize: 20 }}>
-              {
-                sortOptions.find(
-                  (option) => option.value === sortOptionSelected
-                ).name
-              }{" "}
-              ({displayedItems.length})
-            </AppText>
-            <TouchableOpacity
-              style={[
-                styles.sortButton,
-                {
-                  backgroundColor: isSortOptionsVisible
-                    ? colors.buttonPrimary
-                    : colors.white,
-                },
-              ]}
-              onPress={() => setIsSortOptionsVisible(!isSortOptionsVisible)}
-            >
-              <MaterialCommunityIcons
-                name="filter-variant"
-                size={30}
-                color={
-                  isSortOptionsVisible ? colors.white : colors.buttonPrimary
-                }
-              />
-            </TouchableOpacity>
-          </View>
-          <View style={styles.sortOptionsView}>
-            {isSortOptionsVisible && (
-              <View style={styles.sortOptions}>
-                {sortOptions.map((sortOption) => (
-                  <View key={sortOption.value}>
-                    <TouchableOpacity
-                      key={sortOption.value}
-                      onPress={() => handleSortOptionChanged(sortOption.value)}
-                    >
-                      <AppText
-                        style={
-                          sortOptionSelected === sortOption.value
-                            ? styles.optionSelected
-                            : styles.option
-                        }
-                      >
-                        {sortOption.name}
-                      </AppText>
-                    </TouchableOpacity>
-                    {sortOption.value !==
-                      sortOptions[sortOptions.length - 1].value && (
-                      <View style={styles.separator} />
-                    )}
-                  </View>
-                ))}
-              </View>
-            )}
-          </View>
-        </View>
-        <ScrollView horizontal>
-          {paidOptions.map((paidOption) => (
-            <RadioButton
-              key={paidOption.value}
-              value={paidOption.value}
-              onPress={() => handlePaidOptionChanged(paidOption.value)}
-              label={paidOption.name}
-              stateValue={paidOptionSelected}
-              style={{ paddingRight: 20 }}
-            />
-          ))}
-        </ScrollView>
-      </View>
+      <SortMenu
+        sortOptionSelected={sortOptionSelected}
+        setSortOptionSelected={setSortOptionSelected}
+        scrollOptionSelected={paidOptionSelected}
+        setScrollOptionSelected={setPaidOptionSelected}
+        sortOptions={sortOptions}
+        scrollOptions={paidOptions}
+        defaultItems={commandItems}
+        displayedItems={displayedItems}
+        setDisplayedItems={setDisplayedItems}
+      />
       {displayedItems.length === 0 && (
         <View style={styles.noCommandContainer}>
           <AppText style={{ textAlign: "center" }}>
@@ -151,55 +81,6 @@ const styles = StyleSheet.create({
   },
   noCommandContainer: {
     paddingHorizontal: 10,
-  },
-  option: {
-    padding: 5,
-    width: "100%",
-    textAlign: "center",
-    color: colors.light,
-  },
-  optionSelected: {
-    padding: 5,
-    fontSize: 20,
-    width: "100%",
-    textAlign: "center",
-    color: colors.white,
-    fontWeight: "bold",
-  },
-  separator: {
-    height: 1,
-    width: "100%",
-    backgroundColor: colors.light,
-    alignSelf: "center",
-  },
-  sortButton: {
-    padding: 5,
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-  },
-  sortMenuContainer: {
-    marginBottom: 0,
-    paddingHorizontal: 10,
-  },
-  sortOptionsView: {
-    backgroundColor: colors.buttonPrimary,
-    borderRadius: 20,
-    borderTopRightRadius: 0,
-    width: 250,
-  },
-  sortTitle: {
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "100%",
-  },
-  sortView: {
-    width: "100%",
-    flexDirection: "column",
-    alignItems: "flex-end",
-  },
-  sortOptions: {
-    padding: 5,
   },
 });
 
